86a8269aa6553b4faa6a6b5632e9aa60263b9024
Joel Watson <joel.watson@sencha.com>
2016-09-27 06:06:29 -0500

    Fixed nav and added month groups with links

--- js/script.js
+++ js/script.js
@@ -50,20 +50,6 @@ $(document).ready(function() {
 			  paginationSpeed : 400,
 			  singleItem:true
 		});
-
-		//SmothScroll
-		$('a[href*=#]').click(function() {
-			if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'')
-			&& location.hostname == this.hostname) {
-					var $target = $(this.hash);
-					$target = $target.length && $target || $('[name=' + this.hash.slice(1) +']');
-					if ($target.length) {
-							var targetOffset = $target.offset().top;
-							$('html,body').animate({scrollTop: targetOffset}, 600);
-							return false;
-					}
-			}
-		});
 		
 		//Subscribe
 		//new UIMorphingButton( document.querySelector( '.morph-button' ) );
